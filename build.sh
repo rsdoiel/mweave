2729195fe5d6dcdd65c6e1d6801cf7c5b3f64746
R. S. Doiel <rsdoiel@gmail.com>
2013-04-27 11:28:31 -0700

    Added a shell script to run the commands to create mw.js

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+echo "This is a shell script executing the commands to bootstrap mw.js"
+echo "Gragging source from origin master"
+git pull origin master
+echo "Running the vi command to pull mw-bootstrap.js out of README.md"
+vi -e -c "21,68wq! mw-bootstrap.js" README.md
+echo "Running mw-bootstrap.js on Markdown-Weave.md"
+node mw-bootstrap.js Markdown-Weave.md > run.sh
+echo "Running the suggested vi commands to make mw.js and mw_test.js"
+. run.sh
+if [ -f "mw.js" ];then
+    echo "Found mw.js"
+else
+    echo "Missing mw.js, something went wrong."
+    exit 1
+fi
+if [ -f "mw_test.js" ]; then
+    node mw_test.js
+else
+    echo "Missing mw_test.js, something went wrong."
+    exit 1
+fi
